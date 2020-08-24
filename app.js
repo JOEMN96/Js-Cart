9e624deb55e41cad4a331b3373b346af8118eb70
Joemn <Aruljoe37@gmail.com>
2020-08-24 22:26:59 +0530

    Html & Css is Done

---
+++ app.js
@@ -0,0 +1,8 @@
+// Variables From DOm
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+console.log(cartOverlay);
