3c7cbf2d674ee8d6111ccc0a1bcb7c7ed8d7135b
Joemn <Aruljoe37@gmail.com>
2020-08-26 00:05:23 +0530

    half of cart functions DONE

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const clearCartBtn = document.querySelector(".clear-cart");
 const cartDOM = document.querySelector(".cart");
 const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
-const carttotal = document.querySelector(".cart-total");
+const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productDOM = document.querySelector(".products-center");
 
@@ -14,6 +14,7 @@ const productDOM = document.querySelector(".products-center");
 
 let cart = [];
 
+let buttonsDOM = [];
 //  <----- Classes --->
 
 // Getting product
@@ -54,7 +55,7 @@ class UI {
             <article class="product">
             <div  class="img-container">
             <img src="${product.image}"  alt="Product1" class="product-img">
-            <button class="bag-btn" data-id="${product.id}"> <i class="fas fa-shopping-cart">Add To Bag</i> </button>
+            <button class="bag-btn" data-id="${product.id}"> <i class="fas fa-shopping-cart"></i>Add To Bag </button>
             </div>
             <h3>${product.title}</h3>
             <h4>$${product.price}</h4>
@@ -68,8 +69,67 @@ class UI {
 
   getbagBtn() {
     const bagBtns = [...document.querySelectorAll(".bag-btn")];
-    console.log(bagBtns);
+    buttonsDOM = bagBtns;
+    bagBtns.forEach(btn => {
+      let id = btn.dataset.id;
+      let incart = cart.find(item => item.id === id); // Future use
+      if (incart) {
+
+        btn.innerText = "InCart";
+        btn.disabled = true;
+      }
+
+      btn.addEventListener('click', e => {
+        e.target.innerText = " InCart"      // adding clicking funcnality to cart
+        e.target.disabled = true;
+        // Getting Product from local storage
+        let cartItem = { ...Storage.getProducts(id), amount: 1 };
+        // Seting up cart array in global scope
+        cart = [...cart, cartItem]; // Already iruka cartitems + newAdded Items
+        // Saving cart to local storage
+        Storage.saveCart(cart);
+        // Setting up UI CartValues
+        this.setCartValues(cart);
+        // Update Ui of Actual CART
+        this.updateCart(cartItem);
+      })
+    })
   }
+
+  setCartValues(cart) {
+    let priceTotal = 0;
+    let cartVal = 0;
+
+    cart.map((item) => {
+
+      priceTotal += item.price * item.amount;
+      cartVal += item.amount;
+    })
+
+    cartTotal.innerText = parseFloat(priceTotal.toFixed(2));
+    cartItems.innerText = cartVal;
+  }
+
+  updateCart(cart) {
+    let div = document.createElement('div');
+    div.classList.add('cart-item');
+    div.innerHTML = `
+            <img src="${cart.image}">
+            <div>
+              <h4>${cart.title}</h4>
+              <h5>$${cart.price}</h5>
+              <span class="remove-item" data-id="${cart.id}">Remove</span>
+            </div>
+            <div>
+              <i class="fas fa-chevron-up" data-id="${cart.id}></i>
+              <p class="item-amount">${cart.amount}</p>
+              <i class="fas fa-chevron-down">data-id="${cart.id}</i>
+            </div> `;
+
+    cartContent.appendChild(div);
+    console.log(cartContent);
+  }
+
 }
 
 // Local storage
@@ -77,6 +137,14 @@ class UI {
 class Storage {
   static setLocalStorage(productArr) {
     localStorage.setItem("products", JSON.stringify(productArr)); // Saving product to Local Storage
+  }
+  static getProducts(id) {
+    let products = JSON.parse(localStorage.getItem('products'));
+    return products.find(products => products.id === id);
+  }
+
+  static saveCart(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart))
   }
 }
 
